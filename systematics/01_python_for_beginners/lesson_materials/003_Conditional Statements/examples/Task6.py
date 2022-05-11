085bf2646d2d666f015a589069efbb477cb37822
Ihor Cheberiak <Groomsha@users.noreply.github.com>
2022-05-11 12:07:49 +0300

    refactoring developer

--- systematics/01_python_for_beginners/lesson_materials/003_Conditional Statements/examples/Task6.py
+++
@@ -1,7 +0,0 @@
-string = input("Введите строку: ")
-
-if string is not None and string != '':
-    num = int(string)
-
-if string:
-    num = int(string)
--- systematics/01_python_for_beginners/lesson_materials/004_Loop Statements/examples/Task5.py
+++
@@ -1,4 +0,0 @@
-password = input('Введите пароль: ').lower()
-alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-for symbol in password:
-    print(alphabet.find(symbol) + 1)
