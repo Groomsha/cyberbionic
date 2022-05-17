5acb9231bb32a3c887b3bef9ccd28f216c592a75
Ihor Cheberiak <Groomsha@users.noreply.github.com>
2022-05-17 13:20:28 +0300

    fix homework 2

--- 03_python_basic/homework/002_ООП - Наследование/main_homework_four.py.py
+++ 03_python_basic/homework/002_ООП - Наследование/main_homework_four.py.py
@@ -2,11 +2,45 @@
 # транспортных средств поля, в наследниках – специфичные для них. Создайте несколько экземпляров.
 # Выведите информацию о каждом транспортном средстве.
 
-width = int(input('Введите ширину прямоугольника: '))
-height = int(input('Введите высоту прямоугольника: '))
 
-for h in range(0, height):
-	print('', end='\n')
+class Cars:
+	def __init__(self, make: str, type: str, year: int, price: int) -> None:
+		self.make: str = make
+		self.type: str = type
+		self.year: int = year
+		self.price: int = price
 
-	for w in range(0, width):
-		print('*', end='')
+	def get_info(self) -> str:
+		return f'Модель машины: {self.make}. Тип кузова: {self.type}. Год выпуска: {self.year}. Цена: {self.price}$.'
+
+
+class Honda(Cars):
+	def __init__(self, make: str, type: str, year: int, price: int, gas: int) -> None:
+		super().__init__(make, type, year, price)
+
+		self.gas: int = gas
+
+	def get_info(self) -> str:
+		info: str = super(Honda, self).get_info()
+		info += f' Обьем бензобака {self.gas}.'
+		return info
+
+
+class Tesla(Cars):
+	def __init__(self, make: str, type: str, year: int, price: int, battery: int) -> None:
+		super().__init__(make, type, year, price)
+
+		self.battery: int = battery
+
+	def get_info(self) -> str:
+		info: str = super(Tesla, self).get_info()
+		info += f' Обьем батарей {self.battery}.'
+		return info
+
+
+if __name__ == '__main__':
+	accord = Honda('Honda Accord', 'Седан', 2010, 6900, 60)
+	model3 = Tesla('Tesla Model 3', 'Седан', 2019, 30000, 900)
+
+	print(accord.get_info(), end='\n')
+	print((model3.get_info()))
--- 03_python_basic/homework/002_ООП - Наследование/main_homework_two.py
+++ 03_python_basic/homework/002_ООП - Наследование/main_homework_two.py
@@ -1,13 +1,50 @@
 # Опишите классы графического объекта, прямоугольника и объекта, который может обрабатывать нажатия мыши.
 # Опишите класс кнопки. Создайте объект кнопки и обычного прямоугольника. Вызовите метод нажатия на кнопку.
 
-number_one = int(input('Введите число для определения его факториала: '))
+from typing import List
 
-result = 1
-for number in range(1, number_one + 1):
-	if number <= number_one:
-		result = result * number
-	else:
-		break
 
-print(f'\nФакториал числа {number_one}! = {result}')
+class Rectangle:
+	def __init__(self) -> None:
+		pass
+
+	def great_rectangle(self, height: int, width: int) -> None:
+		for h in range(0, height):
+			print('', end='\n')
+
+			for w in range(0, width):
+				print('+', end='')
+
+		print('\n')
+
+
+class NewObject:
+	def __init__(self) -> None:
+		self.mouse_x: float = 0
+		self.mouse_y: float = 0
+
+	def push(self, mouse_touch: List):
+		self.mouse_x = mouse_touch[0]
+		self.mouse_y = mouse_touch[1]
+
+		print(f'Нажатие по кнопке в координатах x:{self.mouse_x} и y:{self.mouse_y} ')
+
+
+class Button(Rectangle, NewObject):
+	def __init__(self, name: str) -> None:
+		super(Rectangle, self).__init__()
+
+		self.name = name
+		self.great_button()
+
+	def great_button(self) -> None:
+		self.great_rectangle(3, 10)
+		print(f'Created -> {self.name}', end='\n')
+
+
+if __name__ == '__main__':
+	rectangle = Rectangle()
+	rectangle.great_rectangle(3, 5)
+
+	button = Button('New button')
+	button.push([23.44, 55.72])
