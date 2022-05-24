20f27155d2fc72506c42e8653498d9fdc59c6a0c
Ihor Cheberiak <Groomsha@users.noreply.github.com>
2022-05-25 00:00:02 +0300

    fix homework 5

--- 03_python_basic/homework/005_Итераторы/main_homework_one.py
+++ 03_python_basic/homework/005_Итераторы/main_homework_one.py
@@ -1,3 +1,11 @@
 # Реализуйте цикл, который будет перебирать все значения итерабельного объекта iterable.
 
-pass
+if __name__ == '__main__':
+	homework = 'Реализуйте цикл, который будет перебирать все значения итерабельного объекта iterable'
+	iterable = iter(homework)
+
+	while True:
+		try:
+			print(next(iterable))
+		except StopIteration:
+			break
